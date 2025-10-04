3ad7c332e267969b769be6a91a5d78be856169fc
gimenodaryll@gmail.com <gimenodaryll@gmail.com>
2025-10-05 00:40:56 +0800

    Company Model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -1,10 +1,31 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+;
 
 class Company extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable= [
+        'name',
+        'email',
+        'logo',
+        'website'
+
+
+
+    ];
+     //Company relationship to Employee
+    public function employees()
+    
+    {
+
+        return $this->hasMany(Employee::class);
+
+    }
+
+    
 }

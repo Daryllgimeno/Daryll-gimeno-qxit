0070c9ee09b186e1ad72d9c31b156745503d8f16
gimenodaryll@gmail.com <gimenodaryll@gmail.com>
2025-10-05 10:58:09 +0800

    Seeding for user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+       // QXIT admin user
+       //use php artisan migrate:fresh --seed for seeding 
+            User::create([
+            'email' => 'qxit@quantumx.com',
+            'password' => Hash::make('interview@qxit'),
         ]);
     }
 }
